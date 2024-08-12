504809aec22b5118c540a71ba65d529b60c6cedd
Suraj-Techuz <suraj@techuz.com>
2024-08-12 18:58:23 +0530

    initial commit

---
+++ src/components/html/Author.js
@@ -0,0 +1,9 @@
+import '../../styles/author.css'
+
+export default function Author() {
+  return (
+    <div className="author">
+      <a target="_blank" rel="noreferrer" href="https://3d-book-r3f.netlify.app/">{`SSJ`}</a>
+    </div>
+  )
+}
